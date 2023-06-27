31ae4706d2dc02149cc682295730e466601cf6c9
mukeshhr22 <msihag028@gmail.com>
2023-06-27 14:29:38 +0530

    one-time-completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,42 @@
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
+$('.move_sl').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 0,
+    prevArrow: $(".previous"),
+    nextArrow: $(".Next_Btn"),
+    // cssEase: 'linear',
+    
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+});
+
+var nightmode = document.getElementById("nightmode")
+nightmode.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("darktheme")) {
+        nightmode.src = "assets/images/png/light.png"
+    } else {
+        nightmode.src = "assets/images/SVG/SETTINGS-ICON.svg"
+            
+    }
 }
 
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
+// *******ROBIN*******
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
 }
